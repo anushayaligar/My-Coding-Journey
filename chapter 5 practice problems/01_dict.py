9279fdc1ad00254aa45b7bff42da605f82ae3f6c
anushayaligar <yaligaranusha17@gmail.com>
2025-09-09 17:12:00 +0530

    Delete chapter 5 practice problems directory

--- chapter 5 practice problems/01_dict.py
+++
@@ -1,9 +0,0 @@
-# dictionary and sets
-d= {}#it is an empty dictionary
-print(type(d))
-marks = {
-    "anu": 100,
-    "kavya" : 90,
-    "Raksh" : 89
-}
-print(marks["anu"])
--- chapter 5 practice problems/05_set_union_intersection.py
+++
@@ -1,5 +0,0 @@
-s = {2,4,5}
-s1 = { 5,6,7} 
-print(s.union(s1))
-
-print(s.intersection(s1))
