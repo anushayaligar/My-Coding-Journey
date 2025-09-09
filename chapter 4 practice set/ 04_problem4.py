6973c20036cb3c3fb4425fa7942b4ec5429325d1
anushayaligar <yaligaranusha17@gmail.com>
2025-09-09 16:19:37 +0530

    Delete chapter 4 practice set directory

--- chapter 4 practice set/ 04_problem4.py
+++
@@ -1,4 +0,0 @@
-# write a program to sum of list of number 4
-a =(32,87,109,67)
-
-print(sum(a))
--- chapter 4 practice set/03_problem3.py
+++
@@ -1,5 +0,0 @@
-# check that a type cannot be changed in python 
-a =(34,56,"Anu")
-a[2] = "baby"
-print(a)
-# no it wont change because of tuple is immutable 
